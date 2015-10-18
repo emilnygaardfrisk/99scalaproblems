5407df6304fba83016916fc43bfed8fe33e4d1dc
Emil Frisk <emilnygaardfrisk@gmail.com>
2015-10-18 21:49:18 +0200

    Solved problem 5

---
+++ src/IsPalindrome.scala
@@ -0,0 +1,7 @@
+/* P06 (*) Find out whether a list is a palindrome. */
+
+object IsPalindrome {
+  val list = List(1, 4, 7, 11);
+  
+  
+}
--- src/ReverseList.scala
+++ src/ReverseList.scala
@@ -1,5 +1,15 @@
 /* P05 (*) Reverse a list. */
 
 object ReverseList {
+  val list = List(1, 4, 7, 11);
   
+  def reverseListRecursive[A](list: List[A], reverseList: List[A] = List()):List[A] = list match {
+    case Nil      => reverseList
+    case h :: tail      => reverseListRecursive(tail, h :: reverseList);
+    case _              => throw new Exception;
+  }
+  
+  def main(args: Array[String]): Unit = {
+    println(reverseListRecursive(list))
+  }
 }
