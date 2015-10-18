b89b375aca8daab9de716f4c491f1a1af009ce22
Emil Frisk <emilnygaardfrisk@gmail.com>
2015-10-18 20:25:29 +0200

    no message

--- src/KthElement.scala
+++ src/KthElement.scala
@@ -1,9 +1,15 @@
 /* P03 (*) Find the Kth element of a list. */
 
 object KthElement {
-  val list = List(1, 4, 7, 11)
+  val list = List(1, 5, 7, 11)
   
-  def kthElement[A](list: List[A]): A = list match {
-    
+  def kthElement[A](list: List[A], k: Int): A = ( list, k ) match {
+    case (h :: tail, k) if k == 0  => h;
+    case (h :: tail, k) if k > 0   => kthElement(tail, k-1);
+    case _                         => throw new NoSuchElementException; 
+  }
+  
+  def main(args: Array[String]): Unit = {
+    println(kthElement(list, 0));
   }
 }
